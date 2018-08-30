73da617e2a5cc01f6059acd1bb8fa1493b825831
mottox2 <y.mottox2@gmail.com>
2018-08-31 02:12:13 +0900

    Fix

--- src/reply.js
+++ src/reply.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context, callback) {
   console.log(webhookBody)
 
   const data = {
-    replyToken: webhookBody.replyToken,
+    replyToken: webhookBody[0].replyToken,
     messages: [
       {
         type: 'text',
