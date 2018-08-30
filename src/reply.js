dbce186380abfa7c816890c780480c3ec71c40a1
mottox2 <y.mottox2@gmail.com>
2018-08-31 02:07:55 +0900

    Use async/await

--- src/reply.js
+++ src/reply.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   const webhookBody = JSON.parse(event.body)
 
   const data = {
@@ -13,12 +13,13 @@ exports.handler = function(event, context, callback) {
     ]
   }
 
-  axios.post('https://api.line.me/v2/bot/message/reply', data, {
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
     }
   })
+  console.log(res)
 
   callback(null, {
     statusCode: 200,
