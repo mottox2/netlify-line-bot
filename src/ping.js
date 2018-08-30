5e6aa25286644d13f74f1fbc5d63268ff8dd22db
mottox2 <y.mottox2@gmail.com>
2018-08-31 00:23:40 +0900

    Fix log

--- src/ping.js
+++ src/ping.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(event, null, 4))
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(event)
